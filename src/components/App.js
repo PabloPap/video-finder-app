e39ae944db0804e2ef18feecf9beed3efdfd130e
Pavlos Papadopoulos <ppapad_33@yahoo.gr>
2019-04-30 20:22:14 +0300

    code and style the list of videos

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,26 @@
 import React from 'react';
 import SearchBar from './SearchBar';
 import youtube from '../apis/youtube';
+import VideoList from './VideoList';
 
 class App extends React.Component {
-  onTermSubmit = term => {
-    youtube.get('/search', {
+  state = {videos: []};
+
+  onTermSubmit = async term => {
+    const response = await youtube.get('/search', {
       params: {
         q: term,
       },
     });
+
+    this.setState({videos: response.data.items});
   };
 
   render() {
     return (
       <div className="ui container">
         <SearchBar onFormSubmit={this.onTermSubmit} />
+        <VideoList videos={this.state.videos} />
       </div>
     );
   }
